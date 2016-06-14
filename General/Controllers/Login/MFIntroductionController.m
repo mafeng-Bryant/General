68fc3466ea7278e1e10c2306fd81e4195d357fdb
feng.ma <feng.ma@patpat.com>
2016-06-14 15:22:47 +0800

    update introduce

--- General/Controllers/Login/MFIntroductionController.m
+++ General/Controllers/Login/MFIntroductionController.m
@@ -12,6 +12,11 @@
 #import "MFGuideSlideBottomView.h"
 
 @interface MFIntroductionController ()<UIScrollViewDelegate>
+{
+    NSArray* _slideDatas;
+    NSInteger _currentPage;
+    CGFloat _lastContentOffsetX;
+}
 @property(nonatomic,strong) UIScrollView* contentScrollView;
 @property(nonatomic,strong) MFGuideSlideViewData* slideData;
 @property(nonatomic,strong) PPPageControl* pageControl;
@@ -42,6 +47,7 @@
 {
     NSArray* datas = [self.slideData datas];
     if (!datas) return;
+    _slideDatas = datas;
     CGFloat height = _bottomView.y - kStatusBarHeight - VMargin30;
     [_contentScrollView.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
     _contentScrollView.contentSize = CGSizeZero;
@@ -49,9 +55,9 @@
         MFGuideSlideViewObject* obj = [datas objectAtIndex:i];
         CGRect rect = CGRectMake(_contentScrollView.width*i, 0, _contentScrollView.width, height);
         MFGuideSlideView* view = [[MFGuideSlideView alloc]initWithFrame:rect object:obj];
+        view.tag = 1000+i;
         view.backgroundColor = [UIColor clearColor];
         [_contentScrollView addSubview:view];
-        
     }
     _contentScrollView.contentSize = CGSizeMake(datas.count*_contentScrollView.width, _contentScrollView.height);
     _pageControl.centerX = self.view.centerX;
@@ -112,11 +118,68 @@
 }
 
 #pragma mark UIScrollView delegate
+
+
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    NSInteger scrollPhotoNumber = MAX(0, MIN(_slideDatas.count-1, (int)(scrollView.contentOffset.x / scrollView.width)));
+    if(scrollPhotoNumber != _currentPage) {
+        _currentPage = scrollPhotoNumber;
+    }
+    float offset =  scrollView.contentOffset.x - (_currentPage * scrollView.width);
+    if(offset < 0) {
+        offset = scrollView.width - MIN(-offset, scrollView.width);
+        CGFloat alpha = offset/scrollView.width;
+        MFGuideSlideView* currentView = (MFGuideSlideView*)[scrollView viewWithTag:1000+_currentPage];
+        [UIView animateWithDuration:0.25 animations:^{
+            currentView.alpha = alpha;
+        }];
+    } else if(offset != 0) {
+        if(scrollPhotoNumber == _slideDatas.count-1) {
+            CGFloat alpha = 1 - offset/scrollView.width;
+            MFGuideSlideView* currentView = (MFGuideSlideView*)[scrollView viewWithTag:1000+_currentPage];
+            [UIView animateWithDuration:0.25 animations:^{
+                currentView.alpha = alpha;
+            }];
+        } else {
+            if (_lastContentOffsetX > scrollView.contentOffset.x)
+            {
+                CGFloat alpha = 1 - offset/scrollView.width;
+                MFGuideSlideView* currentView = (MFGuideSlideView*)[scrollView viewWithTag:1000+_pageControl.currentPage];
+                MFGuideSlideView* preView = (MFGuideSlideView*)[scrollView viewWithTag:1000+_pageControl.currentPage-1];
+                [UIView animateWithDuration:0.25 animations:^{
+                    preView.alpha = alpha;
+                    currentView.alpha = 1- alpha;
+                }];
+                
+            }else if (_lastContentOffsetX < scrollView.contentOffset.x)
+         {
+                CGFloat alpha = 1 - offset/scrollView.width;
+                MFGuideSlideView* currentView = (MFGuideSlideView*)[scrollView viewWithTag:1000+_pageControl.currentPage];
+                MFGuideSlideView* nextView = (MFGuideSlideView*)[scrollView viewWithTag:1000+_pageControl.currentPage+1];
+                [UIView animateWithDuration:0.25 animations:^{
+                    currentView.alpha = alpha;
+                    nextView.alpha = 1- alpha;
+                }];
+                
+            }
+            _lastContentOffsetX = scrollView.contentOffset.x;
+        }
+    }
+}
+
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
 {
     NSInteger currentPage = (scrollView.contentOffset.x  + scrollView.frame.size.width )/ scrollView.frame.size.width-1;
     currentPage = MAX(currentPage,0);
     [self setCurrentPage:currentPage];
+    [UIView animateWithDuration:0.25 animations:^{
+        for (UIView* view in scrollView.subviews) {
+            if ([view isKindOfClass:[MFGuideSlideView class]]) {
+                view.alpha = 1.0;
+            }
+        }
+     }];
 }
 
 - (void)setCurrentPage:(NSInteger)page
